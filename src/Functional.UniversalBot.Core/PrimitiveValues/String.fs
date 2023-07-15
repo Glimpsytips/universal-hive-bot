d57cad44876f65c53ed43b43325e03166902db35
Szymon Sasin <szymon@be-functional.uk>
2023-07-15 18:54:32 +0100

    fixed post publishing issue

--- src/Functional.UniversalBot.Core/PrimitiveValues/String.fs
+++ src/Functional.UniversalBot.Core/PrimitiveValues/String.fs
@@ -22,3 +22,6 @@ let generateRandomString numebrOfCharacters =
     let chars = "0123456789abcdefghijklmnopqrstuvwxyz".ToCharArray()
     let sz = Array.length chars in
     String(Array.init numebrOfCharacters (fun _ -> chars.[randomizer.Next sz])).ToString()
+
+let toLower (input: string) = 
+    input.ToLower()
